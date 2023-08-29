54dc95b1845b1a5d50c00511464b8e747d0d1014
Liz Aryslanova <connect.with.liza@gmail.com>
2023-08-30 01:58:57 +0400

    Simple gradient descent example

--- code_snippets/learning_pytorch.py
+++ code_snippets/learning_pytorch.py
@@ -170,4 +170,108 @@ for epoch in range(3):
 
     print(weights.grad)
 
+    # Nulls the gradients for each next epoch!!!
     weights.grad.zero_()
+
+print('=============================')
+
+
+
+
+'''
+    ==========================
+    BACKPROPAGATION example
+    ==========================
+'''
+
+print('Backprop example')
+
+x = torch.tensor(1.0)
+y = torch.tensor(2.0)
+
+w = torch.tensor(1.0, requires_grad=True)
+
+
+#forward pass
+
+y_hat = w * x
+loss = (y_hat - y)**2
+
+print('loss = ', loss)
+
+
+#backwards pass
+
+loss.backward()
+print(w.grad)
+
+# update weights ...
+# next forward and backward pasees ...
+
+
+
+print('=============================')
+
+
+
+'''
+
+        Gradient descent
+
+'''
+
+print('Gradient descent')
+
+
+
+
+X = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
+Y = torch.tensor([2, 4, 6, 8], dtype=torch.float32)
+
+w = torch.tensor(0.0, dtype=torch.float32, requires_grad=True)
+
+
+# model prediction
+
+def forward(x):
+    return w * x
+
+# loss = MSE
+
+def loss(y, y_predicted):
+    return ( (y_predicted - y)**2).mean()
+
+print(f'Prediction before training: f(5) =  {forward(5):.3f}')
+
+# TRAINING
+
+learning_rate = 0.01
+n_iters = 100
+
+for epoch in range(n_iters):
+    # predicton = forward pass
+    y_pred = forward(X)
+
+    #loss
+    l = loss(Y, y_pred)
+
+    # gradients = backward pass
+    l.backward()  # dl/dw
+
+    # update weights, should not be part of gradient tracking
+    with torch.no_grad():
+        w -= learning_rate * w.grad
+
+    # empty the gradients
+    w.grad.zero_()
+
+    if epoch % 10 == 0:
+        print(f'epoch {epoch + 1}: w = {w:.3f}, loss = {l:.8f}')
+
+print(f'Prediction after training: f(5) = {forward(5):.3f}')
+
+
+
+print('=============================')
+print('=============================')
+
